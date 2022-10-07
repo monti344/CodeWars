8f1b4e62d61258e2cf560338f85943bce28da1fe
Vlad <79315491+monti344@users.noreply.github.com>
2022-10-07 09:33:37 +0300

    Create RegularBallSuperBall.js

---
+++ RegularBallSuperBall.js
@@ -0,0 +1,8 @@
+var Ball = function(ballType) {
+    ballType === undefined ? this.ballType = 'regular' : this.ballType = 'super'
+};
+
+// Top solution
+var Ball = function(ballType) {
+    this.ballType = ballType || 'regular';
+};
